be052cd7e30d1d73c083b8b7d053e3aaed2c16e2
Bartek Walkowicz <wallko1@hotmail.com>
2017-08-02 13:05:53 +0200

    fix import of apache commons lang in CowboyServerCodegen
    * org.apache.commons.lang.StringUtils -> org.apache.commons.lang3.StringUtils

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CowboyServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CowboyServerCodegen.java
@@ -16,7 +16,7 @@ import io.swagger.util.Yaml;
 import java.io.File;
 import java.util.*;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
